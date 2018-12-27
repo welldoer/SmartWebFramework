fafb1c2c03b264b066881d57493a8494d2de8b27
welldoer <welldoer@263.net>
2018-12-27 22:36:50 +0800

    添加 Sqlite 相关的定义，创建 DB 及 Table

---
+++ src/test/java/Connectdatabase.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+import org.junit.jupiter.api.Test;   
+
+
+public class Connectdatabase {
+
+        Connection con = null;
+
+        @Test
+        public void testDB() {
+        	ConnecrDb();
+        }
+        
+        public static Connection ConnecrDb(){
+
+            try{
+                //String dir = System.getProperty("user.dir");
+                Class.forName("org.sqlite.JDBC");
+                Connection con = DriverManager.getConnection("jdbc:sqlite:build/sample.db");
+                System.out.println( "ok!");
+                Statement statement = con.createStatement();
+                ResultSet resultSet = statement.executeQuery( "select * from customer where id = 1;" );
+                
+                return con;
+            }
+            catch(ClassNotFoundException | SQLException e){
+                JOptionPane.showMessageDialog(null,"Problem with connection of database");
+                return null;
+            }
+        }
+
+    }
