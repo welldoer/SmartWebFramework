ca2c92b0afc2fa8ad16bb3288b0672e91cfb83db
welldoer <welldoer@263.net>
2018-12-28 12:06:44 +0800

    现有配置与 JUnit 5 组合，就会出现 :jacocoTestReport SKIPPED，故改回 JUnit 4.

--- src/test/java/net/blogjava/smart4j/service/CustomerServiceTest.java
+++ src/test/java/net/blogjava/smart4j/service/CustomerServiceTest.java
@@ -1,20 +1,14 @@
 package net.blogjava.smart4j.service;
 
-//import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import net.blogjava.smart4j.model.Customer;
 
@@ -22,14 +16,12 @@ public class CustomerServiceTest {
 	
 	private CustomerService customerService;
 
-//	@Before
-	@BeforeEach
+	@Before
 	public void setUp() throws Exception {
 		customerService = new CustomerService();
 	}
 
-//	@After
-	@AfterEach
+	@After
 	public void tearDown() throws Exception {
 	}
 
