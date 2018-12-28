bbccb85ae8c840077232a89d12d6c20adefcb1a5
welldoer <welldoer@263.net>
2018-12-28 13:02:37 +0800

    引入 DbUtils 的 QueryRunner 对象来面向 Entity 进行查询。

--- src/main/java/net/blogjava/smart4j/helper/DatabaseHelper.java
+++ src/main/java/net/blogjava/smart4j/helper/DatabaseHelper.java
@@ -3,8 +3,11 @@ package net.blogjava.smart4j.helper;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +16,8 @@ import net.blogjava.smart4j.util.PropsUtil;
 public final class DatabaseHelper {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger( DatabaseHelper.class );
+	
+	private static final QueryRunner QUERY_RUNNER = new QueryRunner();
 
 	private static final String DRIVER;
 	private static final String URL;
@@ -48,6 +53,19 @@ public final class DatabaseHelper {
 				LOGGER.error( "close connection failure", e );
 			}
 		}
+	}
+	
+	public static <T> List<T> queryEntityList( Class<T> entityClass, Connection conn, String sql, Object... params ) {
+		List<T> entityList;
+		try {
+			entityList = QUERY_RUNNER.query( conn, sql, new BeanListHandler<T>( entityClass ), params );
+		} catch( SQLException e ) {
+			LOGGER.error( "query entity list failure", e );
+			throw new RuntimeException( e );
+		} finally {
+			closeConnection( conn );
+		}
+		return entityList;
 	}
 	
 }
--- src/main/java/net/blogjava/smart4j/service/CustomerService.java
+++ src/main/java/net/blogjava/smart4j/service/CustomerService.java
@@ -19,30 +19,13 @@ public class CustomerService {
 	private static final Logger LOGGER = LoggerFactory.getLogger( CustomerService.class );
 	
 	public List<Customer> getCustomerList( String keyword ) {
-		Connection conn = null;
+		Connection conn = DatabaseHelper.getConnection();
 		try {
-			List<Customer> customerList = new ArrayList<>();
 			String sql = "SELECT * FROM customer";
-			conn = DatabaseHelper.getConnection();
-			PreparedStatement stmt = conn.prepareStatement( sql );
-			ResultSet rs = stmt.executeQuery();
-			while( rs.next() ) {
-				Customer customer = new Customer();
-				customer.setId( rs.getLong( "id" ) );
-				customer.setName( rs.getString( "name" ) );
-				customer.setContact( rs.getString( "contact" ) );
-				customer.setTelephone( rs.getString( "telephone" ) );
-				customer.setEmail( rs.getString( "email" ) );
-				customer.setRemark( rs.getString( "remark" ) );
-				customerList.add( customer );
-			}
-			return customerList;
-		} catch( SQLException e ) {
-			LOGGER.error( "execute sql failure", e );
+			return DatabaseHelper.queryEntityList( Customer.class, conn, sql );
 		} finally {
 			DatabaseHelper.closeConnection( conn );
 		}
-		return null;
 	}
 	
 	public Customer getCustomer( long id ) {
